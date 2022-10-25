f7adff1804bc9bc74058db2befc314a67d55569c
Faisal ahmed <faisal2410@yahoo.com>
2022-10-25 20:08:44 +0600

    updated

--- models/user.js
+++ models/user.js
@@ -85,4 +85,40 @@ const userSchema=mongoose.Schema({
 },{timestamps:true,versionKey:false}
 );
 
+userSchema.pre("save", function (next) {
+  if (!this.isModified("password")) {
+    //  only run if password is modified, otherwise it will change every time we save the user!
+    return next();
+  }
+  const password = this.password;
+
+  const hashedPassword = bcrypt.hashSync(password);
+
+  this.password = hashedPassword;
+  this.confirmPassword = undefined;
+
+  next();
+});
+
+userSchema.methods.comparePassword = function (password, hash) {
+  const isPasswordValid = bcrypt.compareSync(password, hash);
+  return isPasswordValid;
+};
+
+userSchema.methods.generateConfirmationToken = function () {
+  const token = crypto.randomBytes(32).toString("hex");
+
+  this.confirmationToken = token;
+
+  const date = new Date();
+
+  date.setDate(date.getDate() + 1);
+  this.confirmationTokenExpires = date;
+
+  return token;
+};
+
+
+
+
 module.exports=mongoose.model("User",userSchema);
